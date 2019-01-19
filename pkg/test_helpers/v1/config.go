cbe6b816c6ee06c5e83dea633b01d19a49a87616
Semyon Novikov <me@sdfgh153.ru>
2019-01-20 00:44:32 +0500

    Move RandomDate to the test_helpers

--- pkg/test_helpers/v1/config.go
+++ pkg/test_helpers/v1/config.go
@@ -1,5 +1,18 @@
 package test_helpers
 
-import "testing/quick"
+import (
+	"math/rand"
+	"testing/quick"
+	"time"
+)
 
 var DefaultConfig = &quick.Config{MaxCount: 100000}
+
+func RandomDate(rand *rand.Rand) time.Time {
+	min := time.Date(1970, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
+	max := time.Date(2070, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
+	delta := max - min
+
+	sec := rand.Int63n(delta) + min
+	return time.Unix(sec, 0)
+}
--- pkg/types/v1/day_test.go
+++ pkg/types/v1/day_test.go
@@ -11,18 +11,9 @@ import (
 	"github.com/nvkv/halp/pkg/test_helpers/v1"
 )
 
-func randomDate() time.Time {
-	min := time.Date(1970, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
-	max := time.Date(2070, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
-	delta := max - min
-
-	sec := rand.Int63n(delta) + min
-	return time.Unix(sec, 0)
-}
-
 func (d Day) Generate(rand *rand.Rand, size int) reflect.Value {
 	day := Day{
-		Date:      randomDate(),
+		Date:      test_helpers.RandomDate(rand),
 		Breakfast: randomMeal(rand),
 		Lunch:     randomMeal(rand),
 		Dinner:    randomMeal(rand),
