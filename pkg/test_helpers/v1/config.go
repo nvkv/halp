4508e32795c7874d04b582ef498878752acd106d
Semyon Novikov <me@sdfgh153.ru>
2019-01-19 14:55:36 +0500

    Fixup tests, add default config for quick

---
+++ pkg/test_helpers/v1/config.go
@@ -0,0 +1,5 @@
+package test_helpers
+
+import "testing/quick"
+
+var DefaultConfig = &quick.Config{MaxCount: 100000}
--- pkg/types/v1/day_test.go
+++ pkg/types/v1/day_test.go
@@ -1,12 +1,14 @@
 package types
 
 import (
-	//	"fmt"
+	"fmt"
 	"math/rand"
 	"reflect"
 	"testing"
 	"testing/quick"
 	"time"
+
+	"github.com/nvkv/halp/pkg/test_helpers/v1"
 )
 
 func randomDate() time.Time {
@@ -29,11 +31,27 @@ func (d Day) Generate(rand *rand.Rand, size int) reflect.Value {
 	return reflect.ValueOf(day)
 }
 
+// This is a basic test, it will test only
+// Wednesday and Friday's Orthodox lent
+func TestLentDetection(t *testing.T) {
+	lent := func(d Day) bool {
+		var shouldBeLenten = false
+		if d.Date.Weekday() == time.Wednesday || d.Date.Weekday() == time.Friday {
+			shouldBeLenten = true
+		}
+		return d.IsLenten() == shouldBeLenten
+	}
+
+	if err := quick.Check(lent, test_helpers.DefaultConfig); err != nil {
+		t.Error(err)
+	}
+}
+
 func TestValidation(t *testing.T) {
-	gen := func(d Day) bool {
+	validate := func(d Day) bool {
 		validation := d.Validate()
 		if d.IsLenten() {
-			var trulyLenten = false
+			var trulyLenten = true
 			for _, meal := range d.AllMeals() {
 				if meal.IsLenten != true {
 					trulyLenten = false
@@ -43,13 +61,19 @@ func TestValidation(t *testing.T) {
 			if trulyLenten {
 				return validation == nil
 			} else {
+				if validation == nil {
+					fmt.Printf("%v is %v\n", d.Date, d.Date.Weekday().String())
+					for _, meal := range d.AllMeals() {
+						fmt.Printf("Meal lent status: %v\n", meal.IsLenten)
+					}
+				}
 				return validation != nil
 			}
 		}
 		return true
 	}
 
-	if err := quick.Check(gen, nil); err != nil {
+	if err := quick.Check(validate, test_helpers.DefaultConfig); err != nil {
 		t.Error(err)
 	}
 }
